9c2539679c0fa8690d467e95db06d2e481803b4e
LiliiaVesrta <versta270695@gmail.com>
2024-06-19 20:11:47 +0200

    init

--- js/task-2.js
+++ js/task-2.js
@@ -1,157 +1,26 @@
-// function getShippingMessage(country, price, deliveryFee) {
-//   const totalPrice = price + deliveryFee;
-//   return `Shipping to ${country} will cost ${totalPrice} credits`;
-// }
-
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-// function formatMessage(message, maxLength) {
-//   if (message.length <= maxLength) {
-//     return message;
-//   } else {
-//     const truncatedMessage = message.slice(0, maxLength) + '...';
-//     return truncatedMessage;
-//   }
-// }
-
-// console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-// console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-// console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-// console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-// function makeArray(firstArray, secondArray, maxLength) {
-//   return firstArray.concat(secondArray).slice(0, maxLength);
-// }
-
-// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-
-// function calcAverageCalories(days) {
-//   if (days.length === 0) {
-//     return 0;
-//   }
-//   let sumCalories = 0;
-//   for (let obj of days) {
-//     sumCalories += obj.calories;
-//   }
-//   return sumCalories / days.length;
-// }
-
-// console.log(
-//   calcAverageCalories([
-//     { day: 'monday', calories: 3010 },
-//     { day: 'tuesday', calories: 3200 },
-//     { day: 'wednesday', calories: 3120 },
-//     { day: 'thursday', calories: 2900 },
-//     { day: 'friday', calories: 3450 },
-//     { day: 'saturday', calories: 3280 },
-//     { day: 'sunday', calories: 3300 },
-//   ])
-// ); // 3180
-
-// console.log(
-//   calcAverageCalories([
-//     { day: 'monday', calories: 2040 },
-//     { day: 'tuesday', calories: 2270 },
-//     { day: 'wednesday', calories: 2420 },
-//     { day: 'thursday', calories: 1900 },
-//     { day: 'friday', calories: 2370 },
-//     { day: 'saturday', calories: 2280 },
-//     { day: 'sunday', calories: 2610 },
-//   ])
-// ); // 2270
-
-// console.log(calcAverageCalories([])); // 0
-// const getUsersWithFriend = (users, friendName) => {
-//   return users.filter(user => user.friends.includes(friendName));
-// };
-// const allUsers = [
-//   {
-//     name: 'Moore Hensley',
-//     friends: ['Sharron Pace'],
-//   },
-//   {
-//     name: 'Sharlene Bush',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//   },
-//   {
-//     name: 'Ross Vazquez',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//   },
-//   {
-//     name: 'Elma Head',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//   },
-//   {
-//     name: 'Carey Barr',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//   },
-//   {
-//     name: 'Blackburn Dotson',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//   },
-//   {
-//     name: 'Sheree Anthony',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//   },
-// ];
-
-// console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
-// // [
-// //   {
-// //     name: "Sharlene Bush",
-// //     friends: ["Briana Decker", "Sharron Pace"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
-
-// console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
-// // [
-// //   {
-// //     name: "Elma Head",
-// //     friends: ["Goldie Gentry", "Aisha Tran"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
-
-// console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
-class Storage {
-  #items;
-  constructor(params) {
-    this.#items = params;
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
   }
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
-  }
-}
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-
-storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem('Scaner');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+];
