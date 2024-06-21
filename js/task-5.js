3b0dcdb311492895ee5e8f82bc4f194262fefaec
LiliiaVesrta <versta270695@gmail.com>
2024-06-21 19:57:41 +0200

    add js-hw7

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.querySelector('body');
+const divWidget = document.querySelector('.widget');
+
+const changeColor = () => {
+  const newColor = getRandomHexColor();
+  colorSpan.textContent = newColor;
+  body.style.backgroundColor = newColor;
+};
+changeColorBtn.addEventListener('click', changeColor);
